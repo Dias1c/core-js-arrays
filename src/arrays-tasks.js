b49814fef7073d0c0784071624dc100807c367d4
Dias1c <dias.kappasov@gmail.com>
2024-12-25 11:02:13 +0500

    feat: createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -360,8 +360,15 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((p, c, i) => {
+    const shouldCreate = i % chunkSize === 0;
+    if (shouldCreate) {
+      p.push([]);
+    }
+    p[p.length - 1].push(c);
+    return p;
+  }, []);
 }
 
 /**
