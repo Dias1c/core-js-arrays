be3fd819fd24a9608922ef5a35fd8f38073e6294
Dias1c <dias.kappasov@gmail.com>
2024-12-25 10:53:10 +0500

    feat: calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -342,8 +342,10 @@ function selectMany(/* arr, childrenSelector */) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((p, c) => {
+    return p + c[0] - c[1];
+  }, 0);
 }
 
 /**
